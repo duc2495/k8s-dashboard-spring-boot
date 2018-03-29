a184344a91bd1d82f5669eaf4938e26bf3412a8f
Nguyen Dinh Duc <vuongtrungduong1995@gmail.com>
2018-03-29 23:29:35 +0700

    creating..

---
+++ src/main/java/kubernetes/client/model/PostgresTemplate.java
@@ -0,0 +1,13 @@
+package kubernetes.client.model;
+
+public class PostgresTemplate {
+	private String name;
+	private String tag;
+	private int port;
+	private String username;
+	private String password;
+	private String dbname;
+	private String volumeSize;
+	private String mountPath;
+	
+}
